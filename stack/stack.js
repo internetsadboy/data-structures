ecdf14a92bc548bdeb10f559362f66e2b8a8b15c
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-03 22:24:23 -0800

    Update stack.js

--- stack/stack.js
+++ stack/stack.js
@@ -1,63 +1,59 @@
 function Stack(){
-    this.top = null;
-    this.count = 0;
+    
+  this.top = null;
+  this.count = 0;
 
-    this.GetCount = function(){
-        return this.count;
+  this.GetCount = function() {
+    return this.count;
+  }
+
+  this.GetTop = function() {
+    return this.top;
+  }
+
+  this.Push = function(data) {
+    var node = {
+      data : data,
+      next : null
     }
+    node.next = this.top;
+    this.top = node;
+    this.count++;
+  }
 
-    this.GetTop = function(){
-        return this.top;
+  this.Peek = function() {
+    if(this.top === null) {
+      return null;
+    } else {
+      return this.top.data;
     }
+  }
 
-    this.Push = function(data){
-        var node = {
-            data : data,
-            next : null
-        }
-
-        node.next = this.top;
-        this.top = node;
-
-        this.count++;
+  this.Pop = function() {
+    if(this.top === null) {
+      return null; 
+    } else {
+      var out = this.top;
+      this.top = this.top.next;
+      if(this.count>0) {
+        this.count--;
+      }
+      return out.data;
     }
+  }
 
-    this.Peek = function(){
-        if(this.top === null){
-            return null;
-        }else{
-            return this.top.data;
-        }
+  this.DisplayAll = function() {
+    if(this.top === null) {
+      return null;
+    } else {
+      var arr = new Array();
+      var current = this.top;
+      for(var i = 0;i<this.count;i++){
+        arr[i] = current.data;
+        current = current.next;
+      }
+      return arr;
     }
-
-    this.Pop = function(){
-        if(this.top === null){
-            return null;
-        }else{
-            var out = this.top;
-            this.top = this.top.next;
-            if(this.count>0){
-                this.count--;
-            }
-
-            return out.data;
-        }
-    }
-
-    this.DisplayAll = function(){
-        if(this.top === null){
-            return null;
-        }else{
-            var arr = new Array();
-
-            var current = this.top;
-            //console.log(current);
-            for(var i = 0;i<this.count;i++){
-                arr[i] = current.data;
-                current = current.next;
-            }
-
-            return arr;
-        }
-    }
+  }
+  
 }
