2cce205ee3ab8e07783be9471b024259f57352e5
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-03 22:01:38 -0800

    Update linked-list.js

--- linked-list/js/linked-list.js
+++ linked-list/js/linked-list.js
@@ -1,42 +1,36 @@
 function LinkedList() {
-	this.head = null;
+  this.head = null;
 }
 
 LinkedList.prototype = {
-
-	constructor: LinkedList,
-
-	add: function(data) {
-
-		var node = {
-			data:data,
-			next:null
-		};
-
-		var current;
-		if(this.head === null) {
-			this.head = node;
-		} else {
-			current = this.head;
-			while(current.next) {
-				current = current.next;
-			}
-			current.next = node;
-		}
-	},
-
-	item: function(index) {
-		if(index > -1) {
-			var current = this.head,
-					i = 0;
-			while(i++ < index && current) {
-				current = current.next;
-			}
-			return (current ? current.data : null);
-		} else {
-			return null;
-		}
-	},
+  constructor: LinkedList,
+  add: function(data) {
+    var node = {
+      data:data,
+      next:null
+    };
+    var current;
+    if(this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = node;
+      }
+    },
+  item: function(index) {
+    if(index > -1) {
+      var current = this.head, i = 0;
+      while(i++ < index && current) {
+      	current = current.next;
+      }
+      return (current ? current.data : null);
+    } else {
+      return null;
+    }
+  },
 
 	remove: function(index) {
 		if(index > -1) {
